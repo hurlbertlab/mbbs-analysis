579a5b58e91b8ef003fca3cb25b0c8795dc9689a
Ivara Goulden <ivaragoulden@gmail.com>
2023-02-28 11:41:49 -0500

    fixing cat trait data

---
+++ merge-traits-cats.R
@@ -0,0 +1,16 @@
+#----
+#
+# Merge NC species traits csv with cat predation csv from Cooper et al. 2012
+#
+#----
+
+traits <- read.csv("data/NC_species_traits.csv", header = T)
+cats <- read.csv("data/Cooper_2012_catpredation.csv", header = T)
+colnames(cats)
+
+library(dplyr)
+
+traits_wcats <- left_join(traits, cats, by = c('english_common_name'='common.name'))
+
+traits_wcats <- traits_wcats %>% select(!genus.species)
+
