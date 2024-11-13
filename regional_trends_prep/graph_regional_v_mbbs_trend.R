2fbd5e1a37a761ff1a41562b3211d07832dbbcbf
IJBG <ivaragoulden@gmail.com>
2024-11-13 17:04:16 -0500

    fig1

--- regional_trends_prep/graph_regional_v_mbbs_trend.R
+++ regional_trends_prep/graph_regional_v_mbbs_trend.R
@@ -5,17 +5,34 @@
 library(dplyr)
 library(stringr)
 
-reg_trends <- read.csv("regional_trends_prep/regional_trendWIP.csv")
+#get regional trends from the usgs data instead of the regional_trendWIP
+reg_trends <- read.csv("regional_trends_prep/species-list.csv")
 mbbs_trends <- read.csv("data/bayes_hierarchical_year_results_61sp.csv")
 
 trends <- left_join(mbbs_trends, reg_trends, by = "common_name") %>%
-  mutate(mean = mean*100) %>%
+  mutate(mean = mean*100,
+         X5.5. = X5.5.*100,
+         X94.5. = X94.5.*100) %>%
   filter(parameter != "a_bar",
          parameter != "sigma")
 
-plot(trends$mean, trends$trend,
+plot(trends$mean, trends$usgs_trend_estimate,
      xlab = "mbbs trends",
      ylab = "regional trends",
-     main = "Mbbs Trends v Regional Trends")
-abline(1,1)
+     xlim = c(-10,12),
+     ylim = c(-10,12),
+     main = "Mbbs Trends v Regional Trends", 
+     col = "black",
+     pch = 16)
+abline(1,1, col = "red4", lty = "dotted")
+#add segment for the regional trend variation
+segments(trends$mean, 
+         trends$usgs_2.5CI,
+         trends$mean,
+         trends$usgs_97.5CI)
+#add segment for the mbbs variation
+segments(trends$X5.5.,
+         trends$usgs_trend_estimate,
+         trends$X94.5., 
+         trends$usgs_trend_estimate)
 
