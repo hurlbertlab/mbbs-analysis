5effc7f862012137610e535e5b6031ad72aacb85
IJBG <ivaragoulden@gmail.com>
2025-03-05 16:24:13 -0500

    create forest sum

---
+++ 1.landcover-sum-forest.R
@@ -0,0 +1,33 @@
+##############################
+#
+# Script to take the nlcd output
+# and sum across forest types 
+# this is not the "most" ideal as the different
+# categories of forests have different effects on 
+# birds eg. deudicious supports more food compared
+# to pine or mixed forest
+# but
+# the nlcd's clasification abilities come with a decent degree
+# of error as it relates to forest type identification
+# and attempts to truth it against satelite data for a couple
+# of stops showed that at this scale
+# those errors would probably affect conclusions
+# therefore all forest types are summed.
+#
+# after summing
+# this script also creates
+# visual plots of change over time for each stop or quarter stop
+#
+##################################
+
+library(dplyr)
+
+landcover <- read.csv("data/nlcd-landcover/nlcd_annual_landtype_bystop.csv") %>%
+  group_by(route, stop_num, year) %>%
+  filter(ijbg_class %in% 
+           c("deciduous forest", "mixed forest", "evergreen forest")) %>%
+  mutate(perc_forest = sum(percent)) %>%
+  distinct(route, stop_num, year, perc_forest)
+
+#now plot
+#the thing to look out for is dips and then peaks for several years, indicative of an overall pixel categorization / some kinda error. There, we ..mayy.. wind up applying some kind of smoothing
