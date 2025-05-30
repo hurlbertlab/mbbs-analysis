93b0691f53000eeb2a9b329154857ac361333102
IJBG <ivaragoulden@gmail.com>
2025-05-30 12:16:44 -0400

    pending habitat association data

---
+++ 1.traits-get-ebird-habitat-association.R
@@ -0,0 +1,12 @@
+########################
+#
+# Get the forest specialization and grassland specialization
+# Habitat association metrics from ebird status and trends
+# as well as early successional forest vs mature forest if that
+# distinction exists
+#
+########################
+
+library(ebirdst)
+
+#okay so ebirdst_habitat() has been deprecated, reached out for help and will return to this file when I've got that further information/download in hand
