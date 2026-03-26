7c6227d76d15a44d16efd998c0ef35ffe30059e7
IJBG <ivaragoulden@gmail.com>
2026-03-26 14:44:19 -0400

    latex formatting, start open code process

---
+++ 4.generate-stable-code-repository-ch1.R
@@ -0,0 +1,15 @@
+#########################
+#
+# Make the stable respository of everything used in ch1
+#
+########################
+
+#list of files that should be copied
+
+#copy files to new respository location
+
+#create and make some testing file to ensure everything is needed in that repository to run the code
+
+#run the testing code
+
+#outside of R, git commit and git push all changes to the repository
