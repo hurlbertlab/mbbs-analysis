d5d43894ab716d54482e5b878ba2ec346ce05210
IJBG <ivaragoulden@gmail.com>
2026-03-28 11:15:53 -0400

    potential repo name

--- 4.generate-stable-code-repository-ch1.R
+++ 4.generate-stable-code-repository-ch1.R
@@ -1,15 +1,20 @@
 #########################
 #
-# Make the stable respository of everything used in ch1
+# Make the stable repository of everything used in ch1
+# ncmbbs-trends-by-traits
 #
 ########################
 
 #list of files that should be copied
+files.list <- c(
+  "data/"
+)
 
-#copy files to new respository location
+#copy files to new repository location
 
 #create and make some testing file to ensure everything is needed in that repository to run the code
 
 #run the testing code
 
 #outside of R, git commit and git push all changes to the repository
+
